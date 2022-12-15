d61ba26f9e747f39403892cfc6da66d1e5a55b7d
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-12-15 05:12:02 -0800

    Initial Commit

---
+++ HackingWithSwiftProject22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackingWithSwiftProject22
+//
+//  Created by user208023 on 12/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
